8d57ffda3885f198f10060ee8f84cfee89704b98
Anucha.tk <anuchataowkaen@gmail.com>
2024-01-11 10:38:05 +0700

    sec #7

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,13 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car', {
+		color: 'red',
+		year: 1950
+	});
+	const cars = await client.hGetAll('car');
+	if (Object.keys.length) console.log('>>>', cars);
+	console.log(`don't has car id}`);
+};
 run();
